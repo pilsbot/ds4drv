e11ecc2f9acf948d07c0f605df58fee4e2bcd081
Daniel Hackbarth <da_ha@mailbox.org>
2023-11-26 20:09:57 +0100

    Make python package ros2 compatible

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from setuptools import setup
 
-readme = open("README.rst").read()
-history = open("HISTORY.rst").read()
+package_name = 'ds4drv'
 
-setup(name="ds4drv",
+setup(name=package_name,
       version="0.5.1",
       description="A Sony DualShock 4 userspace driver for Linux",
       url="https://github.com/chrippa/ds4drv",
       author="Christopher Rosell",
       author_email="chrippa@tanuki.se",
       license="MIT",
-      long_description=readme + "\n\n" + history,
       entry_points={
         "console_scripts": ["ds4drv=ds4drv.__main__:main"]
       },
+      data_files=[
+          ('share/ament_index/resource_index/packages',
+              ['resource/' + package_name]),
+          ('share/' + package_name, ['package.xml']),
+      ],
       packages=["ds4drv",
                 "ds4drv.actions",
                 "ds4drv.backends",
                 "ds4drv.packages"],
       install_requires=["evdev>=0.3.0", "pyudev>=0.16"],
+      maintainer='Daniel Hackbarth',
+      maintainer_email='da_ha@mailbox.org',
       classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
